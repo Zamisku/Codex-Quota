684b5c006388d72102008d7a5f3e0bc4c0938fac
Zamisku <hoangkhue65yy@gmail.com>
2026-07-12 22:28:01 +0800

    Initial Commit

---
+++ Codex-Quota/Codex_QuotaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Codex_QuotaApp.swift
+//  Codex-Quota
+//
+//  Created by 林毅晨 on 2026/7/12.
+//
+
+import SwiftUI
+
+@main
+struct Codex_QuotaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Codex-Quota/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Codex-Quota
+//
+//  Created by 林毅晨 on 2026/7/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
